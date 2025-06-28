21306f9653a87fb70cbd59208fda703902bd4273
imprashanttt <gm932777@gmail.com>
2025-06-29 00:02:02 +0530

    Added .gitignore to protect sensitive files

--- _02_blog_post/settings.py
+++ _02_blog_post/settings.py
@@ -136,4 +136,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'prashu8511@gmail.com'
-EMAIL_HOST_PASSWORD = "zvep pvsq juuq ynck"  # Not your Gmail password
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
